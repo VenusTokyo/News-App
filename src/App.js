2e1014af9965c788b1aa24d0371209f575a152fe
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2022-12-21 11:23:54 +0530

    added categories :dizzy:

--- src/App.js
+++ src/App.js
@@ -4,13 +4,34 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { Component } from 'react'
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Router>
+        <div>
         <Navbar/>
-        <News pageSize={6}/>
+        
+        <Switch>
+          <Route exact path="/"><News pageSize={9} key="general" country="in" category="general"/></Route>
+          <Route exact path="/business"><News pageSize={9} key="business" country="in" category="business"/></Route>
+          <Route exact path="/entertainment"><News pageSize={9} key="entertainment" country="in" category="entertainment"/></Route>
+          <Route exact path="/general"><News pageSize={9} key="general" country="in" category="general"/></Route>
+          <Route exact path="/health"><News pageSize={9} key="health" country="in" category="health"/></Route>
+          <Route exact path="/science"><News pageSize={9} key="science" country="in" category="science"/></Route>
+          <Route exact path="/sports"><News pageSize={9} key="sports" country="in" category="sports"/></Route>
+          <Route exact path="/technology"><News pageSize={9} key="technology" country="in" category="technology"/></Route>
+          
+        </Switch>
+        </div>
+          
+        </Router>
       </div>
     )
   }
--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
+    static defaultProps={
+        country: "in",
+        pageSize: 9,
+        category: "general"
+    }
+    static propTypes={
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string
+    }
 
     constructor() {
         super();
@@ -15,7 +27,7 @@ export class News extends Component {
         }
     }
     async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=df3b674ce85e488bb1dd1b8acfbfde5d&page=1&pageSize=${this.props.pageSize}`
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=df3b674ce85e488bb1dd1b8acfbfde5d&page=1&pageSize=${this.props.pageSize}`
         this.setState({ loading: true })
 
         let data = await fetch(url);
@@ -34,7 +46,7 @@ export class News extends Component {
         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
 
 
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=df3b674ce85e488bb1dd1b8acfbfde5d&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
+            let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=df3b674ce85e488bb1dd1b8acfbfde5d&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
             this.setState({ loading: true })
             let data = await fetch(url);
             let parsedData = await data.json()
@@ -52,7 +64,7 @@ export class News extends Component {
     handlePrevClick = async () => {
         console.log('prev')
 
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=df3b674ce85e488bb1dd1b8acfbfde5d&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
+        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=df3b674ce85e488bb1dd1b8acfbfde5d&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
         this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json()
@@ -79,7 +91,7 @@ export class News extends Component {
                         </div>
                     })}
                 </div>
-                <div class="d-flex justify-content-between">
+                <div className="d-flex justify-content-between">
 
                     <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
                     <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
