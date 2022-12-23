ba64a1a51b0fd508fe405282d2237b7788acdd06
Shrutika Shaw <66830887+VenusTokyo@users.noreply.github.com>
2022-12-23 12:54:29 +0530

    Updates

---
+++ precache-manifest.d5c06d17865a1ecac21283a0d427e6c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e6d85c8fb493ff6af842dbb9e1fb0d",
+    "url": "/News-App/index.html"
+  },
+  {
+    "revision": "29a9bdca96416b191783",
+    "url": "/News-App/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1950fdaaa95e845552ac",
+    "url": "/News-App/static/js/2.881d2ca0.chunk.js"
+  },
+  {
+    "revision": "29a9bdca96416b191783",
+    "url": "/News-App/static/js/main.82d3f52b.chunk.js"
+  },
+  {
+    "revision": "d3ff2b7b601b67729553",
+    "url": "/News-App/static/js/runtime~main.88486fa2.js"
+  },
+  {
+    "revision": "5780b10b3880805e76a4d87ce14c8d6f",
+    "url": "/News-App/static/media/loading.5780b10b.gif"
+  }
+]);
